40e7e0ddddcded193cb12e1aeae3116b410f1f58
nodexs0 <nodexs@outlook.com>
2024-06-05 00:25:47 -0600

    feat: Add link to lista_cursos page in alumno_home.html template
    This commit updates the alumno_home.html template in the autentificar/templates directory. It adds a link to the lista_cursos page, allowing students to easily access and view their courses.

--- saes/evaluacion/urls.py
+++ saes/evaluacion/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('pregunta/create/', views.create_pregunta, name='create_pregunta'),
     path('preguntas/', views.list_preguntas, name='list_preguntas'),
     path('evaluacion_docente/', views.evaluacion_docente, name='evaluacion_docente'),
-    path('evaluacion_alumno/', views.evaluacion_alumno, name='evaluacion_alumno')
+    path('evaluacion_alumno/', views.evaluacion_alumno, name='evaluacion_alumno'),
+    path('respuesta_create/<str:codigo_curso>/', views.respuesta_create, name='crear_respuesta'),
 ]
--- saes/evaluacion/views.py
+++ saes/evaluacion/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
-from .forms import PreguntaForm
-from .models import Pregunta
+from .forms import PreguntaForm, RespuestaForm
 from administracion.models import Curso, Inscripcion
+from .models import Pregunta, Respuesta
 
 def is_alumno(user):
     return hasattr(user, 'alumno')
@@ -34,17 +34,27 @@ def list_preguntas(request):
 @login_required
 @user_passes_test(is_docente)
 def evaluacion_docente(request):
-    cursos = Curso.objects.filter(docente=request.user)
+    cursos = Curso.objects.filter(docente=request.user.docente)
     return render(request, 'evaluacion_docente.html', {'cursos': cursos})
 
 @login_required
 @user_passes_test(is_alumno)
 def evaluacion_alumno(request):
     cursos_inscritos = Inscripcion.objects.filter(alumno=request.user.alumno)
-    cursos = []
-
-    for inscripcion in cursos_inscritos:
-        if inscripcion.evaluacion_docente != True:
-            cursos.append(inscripcion.curso)
-
+    cursos = [inscripcion.curso for inscripcion in cursos_inscritos if not inscripcion.evaluacion_docente]
     return render(request, 'lista_cursos.html', {'cursos': cursos})
+
+@login_required
+@user_passes_test(is_alumno)
+def respuesta_create(request, codigo_curso):
+    curso = get_object_or_404(Curso, codigo_curso=codigo_curso)
+    if request.method == 'POST':
+        respuesta_form = RespuestaForm(request.POST)
+        if respuesta_form.is_valid():
+            respuesta = respuesta_form.save(commit=False)
+            respuesta.curso = curso
+            respuesta.save()
+            return redirect('evaluacion_alumno')
+    else:
+        respuesta_form = RespuestaForm()
+    return render(request, 'evaluacion_pregunta.html', {'respuesta_form': respuesta_form, 'curso': curso})
